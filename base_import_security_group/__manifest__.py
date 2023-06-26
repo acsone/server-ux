0828ba5100e68c4acba02708f30628a3fca91454
sbejaoui <souheil.bejaoui@acsone.eu>
2023-06-26 09:56:35 +0200

    [16.0][MIG] - base_import_security_group

--- base_import_security_group/__manifest__.py
+++ base_import_security_group/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Optional CSV import",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "category": "Server tools",
     "summary": "Group-based permissions for importing CSV files",
     "license": "AGPL-3",
